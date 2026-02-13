556a4975ba6e52c59a41a085a9e420d6fe17caf7
Caleb TUYISINGIZE <calebmevis11@gmail.com>
2026-02-13 17:43:54 +0200

    .

---
+++ src/components/Header.tsx
@@ -0,0 +1,12 @@
+import svg from '../../public/globe-icon-svg-download-png-2267340.png'
+export default function Header(){
+    return (
+        <>
+        <header>
+            <img src={svg} alt="" />
+            <h1>my travel journal</h1>
+        </header>
+        
+        </>
+    )
+}

20311af4669250e3acac79cbcbb7bd7ed6ad138c
Caleb TUYISINGIZE <calebmevis11@gmail.com>
2026-02-16 16:24:51 +0200

    Driving all data in a component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,24 @@
 import Entry from "./components/Entry"
 import Header from "./components/Header"
+import data from "./data"
+
 
 function App() {
   return (
     <>
     <Header />
-    <Entry />
+    {data.map(el=>{
+      return <Entry key={el.id}
+       img={el.img.src}
+       alt={el.img.alt}
+       title={el.title}
+       country={el.country}
+       googleMapsLink = {el.googleMapsLink}
+       dates = {el.dates}
+       text = {el.dates}     
+              
+       />
+    })}
 
     </>
   )
--- src/components/Entry.tsx
+++ src/components/Entry.tsx
@@ -1,20 +1,31 @@
+type propType = {
 
-export default function Entry(){
+    img: string
+    title: string
+    country: string
+    googleMapsLink: string
+    dates: string
+    text: string
+    alt: string
+}
+
+
+export default function Entry({img, title, country, googleMapsLink, dates, text, alt}: propType){
     return(<>
     <article className="flex p-12">
         <div className="w-imageW shrink-0 rounded-md h-imageH overflow-hidden">
-        <img className="w-imageWFull object-cover  h-imageFull" src="https://scrimba.com/links/travel-journal-japan-image-url" alt="mount fuji" />
+        <img className="w-imageWFull object-cover  h-imageFull" src={img} alt={alt} />
         </div>
         <div className="mt-onInfo mx-5">
             <div className="flex gap-2">
 
             <img className="w-6 "  src="../../public/icons/map-marker-2-svgrepo-com.png" alt="map marker icon" />
-            <span className="uppercase">Japan</span>
-            <a className="underline mt-1mk leading- text-sm text-linkColor" href="https://www.google.com/maps/place/Mount+Fuji/@35.3606421,138.7170637,15z/data=!3m1!4b1!4m6!3m5!1s0x6019629a42fdc899:0xa6a1fcc916f3a4df!8m2!3d35.3606255!4d138.7273634!16zL20vMGNrczA?entry=ttu">View on Google Maps</a>
+            <span className="uppercase">{country}</span>
+            <a className="underline mt-1mk leading- text-sm text-linkColor" href={googleMapsLink}>View on Google Maps</a>
             </div>
-            <h1 className="font-bold mt-2 text-3xl tracking-tighter">Mount Fuji</h1>
-            <p className="font-semibold mt-4">12 Jan, 2021 - 24 Jan, 2021</p>
-            <p className="text-sm mt-4 leading-relaxed mineDisp ">Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.</p>
+            <h1 className="font-bold mt-2 text-3xl tracking-tighter">{title}</h1>
+            <p className="font-semibold mt-4">{dates}</p>
+            <p className="text-sm mt-4 leading-relaxed mineDisp ">{text}</p>
         </div>
         
     </article>
